a8a96923b91e3aaf9ea80a8cc8cd067d55242fc9
Олег <oleg@osvoj.ru>
2022-10-03 16:19:02 +0300

    Написали Hello World!

---
+++ 000/hello.c
@@ -0,0 +1,16 @@
+/*!
+\authors   Олег Лесков <oleg@oSvoj.ru>
+\version   0.0.1a
+\date      10.2022
+\copyright MIT
+*/
+
+/// Подключаем стандартную библиотеку
+#include <stdio.h>
+
+int hello (void) {
+    /// Вывовдим в стандартный вывод надпись \
+        Hello World!!!
+    printf ("Hello world!!!\n");
+    return 0;
+}
