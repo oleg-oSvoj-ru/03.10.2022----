6ef73abf524195449fc0eb6a0b6de0f6a5fc95d3
Олег <oleg@osvoj.ru>
2022-10-05 10:25:07 +0300

    Фиксация шаблона для проектов.

---
+++ 001/001.c
@@ -0,0 +1,16 @@
+/*!
+\authors   Олег Лесков <oleg@oSvoj.ru>
+\version   0.0.1a
+\date      10.2022
+\copyright MIT
+*/
+
+/// Подключаем стандартную библиотеку
+#include <stdio.h>
+
+int start (void) {
+    /// Вывовдим в стандартный вывод надпись \
+        Hello Start!!!
+    printf ("Hello start!!!\n");
+    return 0;
+}
---
+++ 001/001.h
@@ -0,0 +1,8 @@
+/*!
+\authors   Олег Лесков <oleg@oSvoj.ru>
+\version   0.0.1a
+\date      10.2022
+\copyright MIT
+*/
+
+int hello (void);
