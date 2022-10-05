fd4117cd3a53eeeb0bf4b4fb341fc475232bf027
Олег <oleg@osvoj.ru>
2022-10-05 11:53:42 +0300

    Пример оператора вевления

--- 001/001.c
+++ 001/001.c
@@ -7,6 +7,7 @@
 
 /// Подключаем стандартную библиотеку
 #include <stdio.h>
+void s001 (int a);
 
 int start (void) {
     /// Вывовдим в стандартный вывод надпись \
@@ -22,5 +23,17 @@ int start (void) {
     printf("\nВведите переменную d: ");
     scanf("%d", &d);
     printf("\nd = %d", d);
+    s001(d);
     return 0;
+}
+void s001 (int a) {
+    if (a > 0){
+        printf ("\nПеременная a: %d , больше 0.",a);
+    }
+    else if (a == 0){
+        printf ("\nПеременная a: %d , равна 0.",a);
+    }
+    else {
+        printf("\nПеременная a: %d , меньше 0.",a);
+    }
 }
